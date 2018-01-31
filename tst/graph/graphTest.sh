e17a031c591f61c223a6f820504e36ef56975fdc
Franck HOUSSEN <franck.houssen@inria.fr>
2018-01-31 16:23:39 +0100

    [BUG FIX] Tests: check number of logs.

--- tst/graph/graphTest.sh
+++ tst/graph/graphTest.sh
@@ -8,5 +8,10 @@ echo "$RUN" # Print run output.
 RUN="$(./graphRun.sh weak)"
 echo "$RUN" # Print run output.
 
+if [[ "$(find . -maxdepth 1 -name '*.log' | wc -l)" -ne "928" ]]; then
+  echo "ERROR: bad nb log - $(find . -maxdepth 1 -name '*.log' | wc -l)"
+  exit 1
+fi
+
 ./graphPlot.sh > ./graphPlot.log 2>&1
 @DIFF_EXECUTABLE@ ./graphPlot.ref ./graphPlot.log
--- tst/laplacian/laplacianTest.sh
+++ tst/laplacian/laplacianTest.sh
@@ -8,5 +8,10 @@ echo "$RUN" # Print run output.
 RUN="$(./laplacianRun.sh weak)"
 echo "$RUN" # Print run output.
 
+if [[ "$(find . -maxdepth 1 -name '*.log' | wc -l)" -ne "928" ]]; then
+  echo "ERROR: bad nb log - $(find . -maxdepth 1 -name '*.log' | wc -l)"
+  exit 1
+fi
+
 ./laplacianPlot.sh > ./laplacianPlot.log 2>&1
 @DIFF_EXECUTABLE@ ./laplacianPlot.ref ./laplacianPlot.log
