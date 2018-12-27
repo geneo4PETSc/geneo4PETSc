54e710eea778876fd539651e8dc8be8edd0addf3
Franck HOUSSEN <franck.houssen@inria.fr>
2018-12-27 15:20:24 +0100

    TravisCI: move ubuntu CI from (old) trusty to (recent) xenial dist.
    MPI and boost are no more buit from source: this saves (CI) time.

--- prq.sh
+++ prq.sh
@@ -8,7 +8,7 @@ trap 'errorMsg' EXIT
 export PRQ_DIR; PRQ_DIR="$(pwd)/prq"
 
 echo "This script is meant to help installing prerequisites."
-echo "Call this script with any of the following arguments: openmpi boost petsc slepc"
+echo "Call this script with any of the following arguments: petsc slepc"
 if [[ "$#" == "0" ]]; then exit 0; fi
 
 echo ""
@@ -30,43 +30,7 @@ do
   echo "**** $OPT ****"
   echo ""
 
-  if [[ "$OPT" == "openmpi" ]]; then
-    if [[ -f "${PRQ_DIR}/local/bin/mpirun" ]]; then echo "done."; continue; fi
-
-    if [[ ! -f openmpi-1.8.5.tar.gz ]]; then wget https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.5.tar.gz; fi
-    tar -xf openmpi-1.8.5.tar.gz
-    pushd openmpi-1.8.5
-
-    echo -e "\\nconfiguring..."
-    ./configure --prefix="${PRQ_DIR}/local" --with-devel-headers --enable-mpi-cxx --enable-mpi-fortran &> prq.openmpi.configure.log
-    tail -n 25 prq.openmpi.configure.log
-    echo -e "\\nbuilding..."
-    make -j                                                                                            &> prq.openmpi.make.log
-    tail -n 25 prq.openmpi.make.log
-    echo -e "\\ninstalling..."
-    make install                                                                                       &> prq.openmpi.install.log
-    tail -n 25 prq.openmpi.install.log
-    popd
-  elif [[ "$OPT" == "boost" ]]; then
-    if [[ -f "${PRQ_DIR}/local/lib/libboost_mpi.a" ]]; then echo "done."; continue; fi
-
-    if [[ ! -f boost_1_61_0.tar.gz ]]; then wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz; fi
-    tar -xf boost_1_61_0.tar.gz
-    pushd boost_1_61_0
-
-    echo -e "\\nbootstraping..."
-    ./bootstrap.sh --prefix="${PRQ_DIR}/local" --with-libraries=mpi,serialization &> prq.boost.bootstrap.log
-    tail -n 25 prq.boost.bootstrap.log
-    cp -f project-config.jam user-config.jam
-    echo "using mpi : ${PRQ_DIR}/local/bin/mpic++ ;" >> user-config.jam
-    echo -e "\\nbuilding..."
-    ./b2 --user-config=user-config.jam                                            &> prq.boost.make.log
-    tail -n 25 prq.boost.make.log
-    echo -e "\\ninstalling..."
-    ./b2 --user-config=user-config.jam install                                    &> prq.boost.install.log
-    tail -n 25 prq.boost.install.log
-    popd
-  elif [[ "$OPT" == "petsc" ]]; then
+  if [[ "$OPT" == "petsc" ]]; then
     if [[ ! -d petsc/.git ]]; then rm -fr petsc; git clone https://bitbucket.org/petsc/petsc; fi
     pushd petsc
     git checkout master
